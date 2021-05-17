979e6b9a08ead22518a9477ed6d884a81e49df77
coolkidmacho <80176717+coolkidmacho@users.noreply.github.com>
2021-05-16 17:11:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -120,16 +120,23 @@ async def send_request(s: aiohttp.ClientSession, bearer: str, name: str) -> None
 
 async def get_droptime(username: str, session: aiohttp.ClientSession) -> int:
     async with session.get(
-            f"https://api.gapple.pw/blocked/{username}"
+            f"https://mojang-api.teun.lol/droptime/{username}"
     ) as r:
         try:
-
             r_json = await r.json()
-            droptime = int(datetime.strptime(r_json['drop_time'],'%Y-%m-%dT%H:%M:%S.%fZ').timestamp())
-            # droptime = r_json["UNIX"]
+            droptime = r_json["UNIX"]
             return droptime
         except:
-            print("Name not dropping")
+            try:
+                prevOwner = input(
+                    f'{Fore.CYAN}What is the current username of the account that owned {username} before this?:   {Fore.RESET}')
+                r = requests.post('https://mojang-api.teun.lol/upload-droptime',
+                                  json={'name': username, 'prevOwner': prevOwner})
+                print(r.text)
+                droptime = r.json()['UNIX']
+                return droptime
+            except:
+                print(f"{Fore.LIGHTRED_EX}Droptime for name not found, make sure you entered the details into the feild correctly!{Fore.RESET}")
 
     # else:
     #     print(f"{Fore.LIGHTRED_EX}Droptime for name not found, Please check if name is still dropping{Fore.RESET}")
