f5ad4c71da184f7101d034bc25f8fd4d3e7940f4
coolkidmacho <80176717+coolkidmacho@users.noreply.github.com>
2021-05-26 11:03:00 -0700

    added webhooks and autonamemc!

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import json
 import os
 import time
 from datetime import datetime
-import warnings
 import aiohttp
 import requests
-from colorama import Fore
-from colorama import init
+from colorama import Fore, Style, init
 
 init(convert=True, autoreset=True)
 
-# os.system("cls" if os.name == "nt" else "clear")
+os.system("cls" if os.name == "nt" else "clear")
 logo = rf"""{Fore.GREEN}
   __  __ __  __  ___ _____    __  __  _ _ ___ ___ ___
 /' _/|  V  |/  \| _ \_   _| /' _/|  \| | | _,\ __| _ \
@@ -19,10 +17,11 @@ logo = rf"""{Fore.GREEN}
 |___/|_| |_|_||_|_|_\ |_|   |___/|_|\__|_|_| |___|_|_\
 """
 print(logo)
-print(Fore.BLUE + "Created by coolkidmacho#0001" + Fore.RESET)
-print(Fore.BLUE + "With the wonderful assistance of Kqzz#0001\n" + Fore.RESET)
-print("Make sure to join https://discord.gg/KweaD6G97f")
-print("If you want to boost or donate message coolkidmacho#0001 on discord")
+print(Fore.LIGHTCYAN_EX + Style.BRIGHT + "Created by Coolkidmacho#0001" + Fore.RESET)
+print(Fore.LIGHTCYAN_EX + "With the wonderful assistance of Kqzz#0001" + Fore.RESET)
+print(f"{Fore.MAGENTA}Make sure to join https://discord.gg/KweaD6G97f\n")
+print(f"{Style.BRIGHT}{Fore.YELLOW}If you want to boost or donate message Coolkidmacho#0001 on discord")
+# print(f"{Style.DIM}This file is fully editable and changable, but all changes must be open source, changing the logo or the credits are against the request of the owner.")
 end = []
 orgdel = 0
 global delay
@@ -33,20 +32,29 @@ global tuned_delay
 tuned_delay = None
 global success
 success = False
-
 reqnum = 3
-
 global mcsauth
 mcsauth = ""
+
+global webhook
+
+
+def inp(text):
+    print(f"{Fore.YELLOW}{text}", end="")
+    ret = input("")
+    return ret
 
 
 def get_config_data():
     with open("config.json") as e:
         menu = json.loads(e.read())
-        print(menu)
+        # print(menu)
         global namemc
         namemc = menu["namemc"]
         msauth = menu["msauth"]
+        webhook = menu["webhook"]
+        if webhook == "":
+            webhook = None
 
 
 get_config_data()
@@ -75,7 +83,7 @@ def store(droptime: int, offset: int) -> None:  # Dodgy timing script!
             f"""{Fore.LIGHTRED_EX}Cannot tune your delay, please sync your time\n
             using http://www.thinkman.com/dimension4/download.htm
             \nprogram will continue, if it fails again please restart after \n
-            installing dimension4 and also set the delay to 0 for that{Fore.RESET}"""
+            installing dimension4 and also set the delay to 0 for that{Fore.RESET}, also you can get this issue if you set your delay to 0"""
         )
 
     else:
@@ -116,21 +124,23 @@ async def send_request(s: aiohttp.ClientSession, bearer: str, name: str) -> None
         )
         end.append(datetime.now())
         if r.status == 200:
+            print(f"{Fore.GREEN}C")
             success = True
 
 
 async def get_droptime(username: str, session: aiohttp.ClientSession) -> int:
     async with session.get(
-            f"https://mojang-api.teun.lol/droptime/{username}"
+            f"https://api.kqzz.me/droptime/{username}"
     ) as r:
         try:
             r_json = await r.json()
+            print(r_json)
             droptime = r_json["UNIX"]
             return droptime
         except:
             try:
-                prevOwner = input(
-                    f'{Fore.CYAN}What is the current username of the account that owned {username} before this?:   {Fore.RESET}')
+                prevOwner = inp(
+                    f'What is the current username of the account that owned {username} before this?:   ')
                 r = requests.post('https://mojang-api.teun.lol/upload-droptime',
                                   json={'name': username, 'prevOwner': prevOwner})
                 print(r.text)
@@ -140,11 +150,6 @@ async def get_droptime(username: str, session: aiohttp.ClientSession) -> int:
                 print(
                     f"{Fore.LIGHTRED_EX}Droptime for name not found, make sure you entered the details into the feild correctly!{Fore.RESET}")
 
-    # else:
-    #     print(f"{Fore.LIGHTRED_EX}Droptime for name not found, Please check if name is still dropping{Fore.RESET}")
-    #     time.sleep(2)
-    #     input(f"{Fore.LIGHTRED_EX}Press Enter to exit: {Fore.RESET}")
-    #     exit()
 
 
 async def snipe(target: str, offset: int, bearer_token: str) -> None:
@@ -168,7 +173,7 @@ async def snipe(target: str, offset: int, bearer_token: str) -> None:
 async def autosniper(bearer: str) -> None:
     print(f"{Fore.LIGHTGREEN_EX}Starting...{Fore.RESET}")
     names = requests.get("https://api.3user.xyz/list").json()
-    delay = input(f"{Fore.CYAN}Delay for snipe:  {Fore.RESET}")
+    delay = inp(f"Delay for snipe:  ")
     if tuned_delay == None:
         pass
     else:
@@ -249,8 +254,8 @@ async def get_mojang_token(email: str, password: str) -> str:
                             print("logged in correctly")
                 else:
                     try:
-                        for x in range(3):
-                            ans = input(resp_json[x]["question"]["question"])
+                        for x in range(2):
+                            ans = inp({resp_json[x]["question"]["question"]})
                             answers.append({"id": resp_json[x]["answer"]["id"], "answer": ans})
                     except IndexError:
                         print(f"{Fore.LIGHTRED_EX}Please provide answers to the security questions{Fore.RESET}")
@@ -286,7 +291,7 @@ async def mojang_snipe(target: str, offset: int, bearer_token: str) -> None:
 async def automojangsniper(token: str) -> None:
     print(f"{Fore.LIGHTGREEN_EX}Starting...{Fore.RESET}")
     names = requests.get("https://api.3user.xyz/list").json()
-    delay = input(f"{Fore.CYAN}Delay for snipe:  {Fore.RESET}")
+    delay = inp(f"Delay for snipe:  ")
     print(tuned_delay, "tuned delay value")
     for nameseg in names:
         name = nameseg["name"]
@@ -301,11 +306,11 @@ async def automojangsniper(token: str) -> None:
 
 
 def getok():
-    input("warning, if you have generated a token in the past 12 hours dont do it again!, click enter to continue...")
-    input(
+    inp("warning, if you have generated a token in the past 12 hours dont do it again!, click enter to continue...")
+    inp(
         "NOTE: IF YOU HAVE NOT INSTALLED THE AUTHENTICATION SERVER GO HERE  ( https://github.com/coolkidmacho/McMsAuth ), THIS WILL NOT WORK WITHOUT IT, click enter to continue")
-    email = input("Microsoft email:  ")
-    password = input("Microsoft Password:  ")
+    email = inp("Microsoft email:  ")
+    password = inp("Microsoft Password:  ")
     myinfo = {
         "email": email,
         "password": password
@@ -320,23 +325,40 @@ def getok():
 
 
 async def gather_mojang_info() -> None:
-    email = input(f"Account Email:  ")
-    password = input(f"Password:  ")
+    email = inp(f"Account Email:  ")
+    password = inp(f"Password:  ")
     print(password)
     token = await get_mojang_token(email, password)
-    style = input(
+    style = inp(
         "What sniper mode? Enter `a` for autosniper"
         " and `n` for single name sniping:  "
     )
     if style == "a":
         await automojangsniper(token)
     elif style == "n":
-        name = input(f"Name to snipe:  ")
-        delay = input(f"Delay for snipe:  ")
+        name = inp(f"Name to snipe:  ")
+        delay = inp(f"Delay for snipe:  ")
         tuned_delay = delay
         await mojang_snipe(name, delay, token)
         if namemc == "True":
             autonamemc(email, password)
+        if webhook != None:
+            webhookjson = {
+                "content": "",
+                "embeds": [
+                    {
+                        "title": f"You sniped {name}",
+                        "description": f"Congratulations on your new snipe!\n\nYou sniped {name.upper()}!",
+                        "color": 5814783,
+                        "footer": {
+                            "text": "Sniper Made by Coolkidmacho#0001"
+                        }
+                    }
+                ],
+                "username": "Smart Sniper",
+                "avatar_url": "https://cdn.discordapp.com/icons/840342619329658921/a_d3e87d7774f9c82b684c3a667e9cf23e.webp?size=128"
+            }
+            result = requests.post(webhook, json=webhookjson)
 
 
 async def iterate_through_names(session: aiohttp.ClientSession) -> None:
@@ -353,9 +375,12 @@ async def iterate_through_names(session: aiohttp.ClientSession) -> None:
 
 
 async def start() -> None:
-    mainset = input(
-        f"What account type? Enter `g` for giftcard snipes "
-        f"and `m` for mojang and `ms` for microsoft accounts, if you want to get your micorsoft/gc token automatically type `v`:  "
+    mainset = inp(
+        f"\n\n{Fore.LIGHTBLUE_EX}What account type? \n"
+        f"{Fore.LIGHTBLUE_EX}Enter {Fore.GREEN}g{Fore.RESET}{Fore.LIGHTBLUE_EX} for giftcard snipes \n"
+        f"{Fore.LIGHTBLUE_EX}Enter {Fore.GREEN}m{Fore.RESET} {Fore.LIGHTBLUE_EX}for mojang snipes \n"
+        f"{Fore.LIGHTBLUE_EX}Enter {Fore.GREEN}ms{Fore.RESET} {Fore.LIGHTBLUE_EX}for microsoft snipes \n"
+        f"{Fore.LIGHTBLUE_EX}Enter {Fore.GREEN}v{Fore.RESET} {Fore.LIGHTBLUE_EX}for auto token:  "
     )
     if mainset == "v":
         getok()
@@ -371,8 +396,8 @@ async def start() -> None:
         print(
             f"{Fore.LIGHTGREEN_EX}Microsoft Account Selected, using Microsoft Sniper{Fore.RESET}"
         )
-        token = input(f"What is your bearer token:  ")
-        style = input(
+        token = inp(f"What is your bearer token:  ")
+        style = inp(
             "What sniper mode? Enter `a` for autosniper"
             " and `n` for single name sniping:  "
         )
@@ -380,33 +405,33 @@ async def start() -> None:
             await automojangsniper(token)
             return
         elif style == "n":
-            name = input(f"Name to snipe:  ")
+            name = inp(f"Name to snipe:  ")
             global delay
-            delay = input(f"Delay for snipe:  ")
+            delay = inp(f"Delay for snipe:  ")
             global tuned_delay
             tuned_delay = delay
             await (mojang_snipe(name, delay, token))
     elif mainset == "g":
 
         reqnum = 6
-        token = input(f"What is your bearer token:  ")
-        style = input(
+        token = inp(f"What is your bearer token:  ")
+        style = inp(
             f"What sniper mode? Enter `a` for autosniper and"
             f" `n` for single name sniping:  "
         )
         if style == "a":
             await autosniper(token)
         elif style == "n":
-            name = input(f"Name to snipe:  ")
-            delay = input(f"Delay for snipe:  ")
+            name = inp(f"Name to snipe:  ")
+            delay = inp(f"Delay for snipe:  ")
             tuned_delay = delay
             await snipe(name, delay, token)
         else:
-            print(f"Please select a valid option")
-            input(f"Press enter to exit: ")
+            print(f"{Fore.RED}Please select a valid option")
+            inp(f"Press enter to exit: ")
             exit()
     else:
-        print("You did not enter a proper value. Ending.")
+        print(f"{Fore.RED}You did not enter a proper value. Ending.")
         exit()
 
 
